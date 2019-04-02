19a741eda07b264966f6e5088d30ba056248df0d
tako_desktop <takotabak@gmail.com>
2019-04-02 14:55:24 +0200

    added better handling of 7z

--- cloudfile/extract.py
+++ cloudfile/extract.py
@@ -3,6 +3,16 @@ import os
 
 def extract(file: str):
     if file[-3:] == ".7z":
-        os.system("7z x " + file)
-    
+        if in_subfolder(file):
+            os.system("7z x " + file + " -oc:" + os.path.join(*file.split(os.sep)[:-1]))
+        else:
+            os.system("7z x " + file)
 
+
+def in_subfolder(file):
+    spllitted = file.split(os.sep)[:-1]
+    if len(spllitted) > 1:
+        return True
+    if spllitted != ["."]:
+        return True
+    return False
--- tests/test_extract.py
+++ tests/test_extract.py
@@ -1,5 +1,7 @@
+import json
 import os
 
+import cloudfile
 from cloudfile.extract import extract
 from tests.utils import del_folder
 
@@ -17,3 +19,34 @@ def test_extract():
     assert os.path.isfile(filename)
     os.remove(filename)
     os.remove(zipfilename)
+
+
+def test_extract_folder():
+    cloudfile_dct = {
+        "foo.7z": "https://drive.google.com/uc?export=download&id=1iU8DK-ZMzHz3fANAcrcYwNW4K71cTjeJ"
+    }
+    json.dump(cloudfile_dct, open("cloudfile.json", "w"))
+
+    cloudfile.restore()
+    assert os.path.isdir("foo")
+    assert os.path.isdir("foo/foo2")
+    assert os.path.isfile("foo/bar.txt")
+    assert os.path.isfile("foo/foo2/bar2.txt")
+    os.remove("cloudfile.json")
+    os.remove("foo.7z")
+    del_folder("foo")
+
+
+def test_extract_subfolder():
+    cloudfile_dct = {
+        "fubar/foo.7z": "https://drive.google.com/uc?export=download&id=1iU8DK-ZMzHz3fANAcrcYwNW4K71cTjeJ"
+    }
+    json.dump(cloudfile_dct, open("cloudfile.json", "w"))
+
+    cloudfile.restore()
+    assert os.path.isdir("fubar")
+    assert os.path.isdir("fubar/foo/foo2")
+    assert os.path.isfile("fubar/foo/bar.txt")
+    assert os.path.isfile("fubar/foo/foo2/bar2.txt")
+    os.remove("cloudfile.json")
+    del_folder("fubar")
