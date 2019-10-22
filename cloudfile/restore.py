7cef837453c2fb5559955777fddc81e9e2b15b62
tako_desktop <takotabak@gmail.com>
2019-10-22 16:12:23 +0200

    less verbose 2

--- cloudfile/restore.py
+++ cloudfile/restore.py
@@ -7,7 +7,7 @@ from .extract import extract
 from .check_downloaded import file_downloaded
 
 
-def restore_file(file: str, cloudf: CloudFile = None, hard=False, verbose=False):
+def restore_file(file: str, cloudf: CloudFile = None, hard=False, verbose=True):
     if cloudf is None:
         cloudf = CloudFile()
     url = cloudf[file]
@@ -15,8 +15,9 @@ def restore_file(file: str, cloudf: CloudFile = None, hard=False, verbose=False)
     path_to = get_path_to(file)
     if not os.path.isdir(path_to):
         os.makedirs(path_to)
+
     if not os.path.isfile(file) or hard:
-        download(file, url)
+        download(file, url, verbose)
     elif verbose:
         print(
             f"Did not download {file}. File already exist.\nRun `cloudfile download {dir}` to download this file anyway."
@@ -31,14 +32,15 @@ def restore(cloudf: CloudFile = None, hard=False):
         restore_file(file, cloudf, hard)
 
 
-def download(dir: str, url: str):
+def download(dir: str, url: str, verbose: bool = True):
     path_to = get_path_to(dir)
     file_dir = wget.download(url, out=path_to)
     os.rename(src=file_dir, dst=dir)
     extract(file_dir)
     # if not file_downloaded(dir):
     #     raise NotImplementedError("File is not downloaded something went wrong.")
-    print(f"\nDownloaded {dir} from {url}")
+    if verbose:
+        print(f"\nDownloaded {dir} from {url}")
 
 
 def get_path_to(path: str):
